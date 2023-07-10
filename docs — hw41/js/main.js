88445032b57c869fa4feedfa7a1061dee6ae7775
Andru6a <136846149+Andru6a@users.noreply.github.com>
2023-07-10 10:27:00 +0300

    Add files via upload

---
+++ docs — hw41/js/main.js
@@ -0,0 +1,47 @@
+$(function () {
+  Fancybox.bind()
+
+  var mixer = mixitup('.directions__list');
+
+  $('.directions__filter-btn').on('click', function () {
+    $('.directions__filter-btn').removeClass('directions__filter-btn--active')
+    $(this).addClass('directions__filter-btn--active')
+  })
+
+
+  $('.team__slider').slick({
+    arrows: false,
+    slidesToShow: 4,
+    infinite: true,
+    draggable: false,
+    waitForAnimate: false,
+    dots:false,
+  })
+  $('.team__slider-prev').on('click', function (e) {
+    e.preventDefault()
+    $('.team__slider').slick('slickPrev')
+  })
+  $('.team__slider-next').on('click', function (e) {
+    e.preventDefault()
+    $('.team__slider').slick('slickNext')
+  })
+
+  $('.testimonials__slider').slick({
+    arrows: false,
+    infinite: true,
+    dots: true,
+    draggable: false,
+    waitForAnimate: false,
+    appendDots: $('.testimonials__dots')
+  })
+  $('.testimonials__prev').on('click', function (e) {
+    e.preventDefault()
+    $('.testimonials__slider').slick('slickPrev')
+  })
+  $('.testimonials__next').on('click', function (e) {
+    e.preventDefault()
+    $('.testimonials__slider').slick('slickNext')
+  })
+
+
+})
